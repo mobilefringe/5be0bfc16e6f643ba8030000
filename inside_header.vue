0ceb8bfddea889b7acdc9e30cddb18d45df831ba
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-08 16:16:38 -0500

    Coral Ridge 1.0  /inside_header.vue

--- inside_header.vue
+++ inside_header.vue
@@ -13,6 +13,22 @@
         return Vue.component("inside-header-component", {
             template: template, // the variable template will be injected,
             props: ['pageName'],
+            created (){
+                this.loadData().then(response => {
+                    this.dataloaded = true;
+                    this.filteredStores = this.allStores;
+                    
+                    // this.storeBanner = this.findRepoByName('Stores Banner').images[0];
+                    var temp_repo = this.findRepoByName('Map Banner');
+                    if(temp_repo) {
+                        this.pageBanner = temp_repo.images[0];
+                    }
+                    else {
+                        this.pageBanner = {};
+                        this.pageBanner.image_url = "";
+                    }
+                });
+            },
             methods:{
                 loadData: async function() {
                     try {
