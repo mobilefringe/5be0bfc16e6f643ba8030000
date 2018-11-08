6ddfefb66c4d39a41464ae3de38df3dbc38b4ef3
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-08 16:27:44 -0500

    Coral Ridge 1.0  /page_details.vue

---
+++ page_details.vue
@@ -0,0 +1,65 @@
+<template>
+    <div> <!-- without an outer container div this component template will not render -->
+        <loading-spinner v-if="!dataLoaded"></loading-spinner>
+        <transition name="fade">
+            <div v-if="dataLoaded" v-cloak>
+                <div class="inside_header_background">
+                    <div class="main_container">
+                        <h2 v-html="currentPage.title"></h2>
+                    </div>
+                </div>
+                <inside-header-component :pageName="currentPage.title"></inside-header-component>
+                <div class="main_container mobile_padding margin_30">
+                    <div class="details_row">
+                        <div class="details_col_3 hidden_phone">
+                            <img class="img_max" src="http://placehold.it/440x1200" alt="" />    
+                        </div>
+                        <div class="details_col_9">
+                            <div class="page_body" v-html="currentPage.body"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+    define(["Vue", "vuex"], function (Vue, Vuex) {
+        return Vue.component("page-details-component", {
+            template: template, // the variable template will be injected,
+            props: ['id'],
+            data: function data() {
+                return {
+                    dataLoaded: false,
+                    currentPage: null
+                }
+            },
+            created() {
+                this.updateCurrentPage(this.id);
+            },
+            watch: {
+                $route: function () {
+                    this.updateCurrentPage(this.$route.params.id);
+                }
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property'
+                ])
+            },
+            methods: {
+                updateCurrentPage(id) {
+                    var _this = this;
+                    this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host + "/pages/" + this.id + ".json" }).then(function (response) {
+                        _this.currentPage = response.data;
+                        _this.dataLoaded = true;
+                    }, function (error) {
+                        console.error( "Could not retrieve data from server. Please check internet connection and try again.");
+                        _this.$router.replace({ name: '404' });
+                    });
+                }
+            }
+        });
+    });
+</script>
