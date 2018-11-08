8dd30b71dc9837e7422905fb145012c06e43d782
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-08 13:43:15 -0500

    Coral Ridge 1.0  /main.js

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ require.config({
         'mousewheel' : 'https://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.min',
         'mapplic' : 'https://mmvue.codecloudapp.com/mapplic',
         'hammer' : 'https://codecloud.cdn.speedyrails.net/sites/59bac7db6e6f644f22ba0000/text/javascript/1484859750000/hammer.min',
-        'mapplic-png-map' : 'http://mmvue.codecloudapp.com/mapplic_png.vue?noext',
+        'mapplic-png-map' : 'https://mmvue.codecloudapp.com/mapplic_png.vue?noext',
         'loader': 'https://mmvue.codecloudapp.com/loader.vue?noext',
     }
 });
