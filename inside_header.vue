f4797e381ca30402ba613b4ef1c4f5f7fe00c050
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-08 16:16:18 -0500

    Coral Ridge 1.0  /inside_header.vue

--- inside_header.vue
+++ inside_header.vue
@@ -12,7 +12,17 @@
     define(["Vue", "vuex"], function (Vue, Vuex) {
         return Vue.component("inside-header-component", {
             template: template, // the variable template will be injected,
-            props: ['pageName']
+            props: ['pageName'],
+            methods:{
+                loadData: async function() {
+                    try {
+                        // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
+                        let results = await Promise.all([this.$store.dispatch("getData", "categories"), this.$store.dispatch("getData", "repos")]);
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                },
+            }
         });
     });
 </script>
