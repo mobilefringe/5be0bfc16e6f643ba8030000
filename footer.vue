d795c6fb17d77301cb165518216c08ad5d28b911
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-20 12:18:39 -0500

    Coral Ridge 1.0  /footer.vue

--- footer.vue
+++ footer.vue
@@ -28,10 +28,10 @@
                     <div class="clearfix"></div>
                 </div>
                 <div class="col-xs-12 col-sm-6 col-md-4 footer_newsletter">
-                    <p class="footer_heading">NEWSLETTER SUBSCRIPTION</p>
-                    <label for="emailAddress" class="accessibility">Enter Email Address</label>
-                    <input id="emailAddress" v-model="newsletter_email" type="text" placeholder="Susbcribe to Newsletter" class="newsletter_control" required />
-                    <button @click="newsletterRoute" class="newsletter_btn animated_btn">Subscribe</button>
+                    <!--<p class="footer_heading">NEWSLETTER SUBSCRIPTION</p>-->
+                    <!--<label for="emailAddress" class="accessibility">Enter Email Address</label>-->
+                    <!--<input id="emailAddress" v-model="newsletter_email" type="text" placeholder="Susbcribe to Newsletter" class="newsletter_control" required />-->
+                    <!--<button @click="newsletterRoute" class="newsletter_btn animated_btn">Subscribe</button>-->
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-4 footer_insta">
                     <a class="pull-right insta_view_more" :href="siteInfo.instagramURL" target="_blank">View More <i class="fa fa-caret-right"></i></a>
