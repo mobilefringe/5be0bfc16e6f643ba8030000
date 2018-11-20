e5ce32be8c2fbd17ab5f9f3bc0598ab19281317a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-20 15:09:50 -0500

    Coral Ridge 1.0  /routes.js

---
+++ routes.js
@@ -0,0 +1,198 @@
+define([], function () {
+    return [
+        {
+            path: '/',
+            component: view('home')
+        },
+        {
+            path: '/contact-us',
+            component: view('default'),
+            children: [
+                {
+                    path: '',
+                    component: view('contact'),
+                    meta: {
+                        pageName: 'Contact Us',
+                    },
+                    name: 'contact'
+                }
+            ]
+        },
+        {
+            path: '/dine',
+            component: view('default'),
+            children: [
+                {
+                    path: '',
+                    component: view('dine'),
+                    meta: {
+                        pageName: 'Dine',
+                    },
+                    name: 'dine'
+                }
+            ]
+        },
+        {
+            path: '/events',
+            component: view('default'),
+            children: [
+                {
+                    path: ':id',
+                    component: view('event_details'),
+                    meta: {
+                        pageName: 'Events',
+                    },
+                    name: 'eventDetails',
+                    props: true
+                }
+            ]
+        },
+        {
+            path: '/jobs',
+            component: view('default'),
+            children: [
+                {
+                    path: '',
+                    component: view('jobs'),
+                    meta: {
+                        pageName: 'Careers',
+                    },
+                    name: 'jobs'
+                },
+                {
+                    path: ':id',
+                    component: view('job_details'),
+                    meta: {
+                        pageName: 'Careers',
+                    },
+                    name: 'jobDetails',
+                    props: true
+                }
+            ]
+        },
+        {
+            path: '/location',
+            component: view('default'),
+            children: [
+                {   
+                    path: '',
+                    component: view('location'),
+                    meta: {
+                        pageName: 'Location',
+                    },
+                    name: 'location'
+                }
+            ]
+        },
+        {
+            path: '/map',
+            component: view('default'),
+            children: [
+                {   
+                    path: '',
+                    component: view('map'),
+                    meta: {
+                        pageName: 'Centre Map',
+                    },
+                    name: 'map'
+                }
+            ]
+        },
+        {
+            path: '/newsletter',
+            component: view('default'),
+            children: [
+                {
+                    path: '',
+                    component: view('newsletter'),
+                    meta: {
+                        pageName: 'Newsletter',
+                    },
+                    name: 'newsletter'
+                }
+            ]
+        },
+        {
+            path: '/pages',
+            component: view('default'),
+            children: [
+                {
+                    path: ':id',
+                    component: view('page_details'),
+                    name: 'pageDetails',
+                    props: true
+                }
+            ]
+        },
+        {
+            path: '/sales-and-events',
+            component: view('default'),
+            children: [
+                {
+                    path: '',
+                    component: view('promotions-and-events'),
+                    meta: {
+                        pageName: 'Promotions & Events',
+                    },
+                    name: 'promotions-and-events'
+                }
+            ]
+        },
+        {
+            path: '/promotions',
+            component: view('default'),
+            children: [
+                {
+                    path: ':id',
+                    component: view('promotion_details'),
+                    meta: {
+                        pageName: 'Promotions',
+                    },
+                    name: 'promotionDetails',
+                    props: true
+                }
+            ]
+        },
+        {
+            path: '/stores',
+            component: view('default'),
+            children: [
+                {   
+                    path: '',
+                    component: view('stores'),
+                    meta: {
+                        pageName: 'Store Directory',
+                    },
+                    name: 'storeList'
+                },
+                {
+                    path: ':id',
+                    component: view('store_details'),
+                    meta: {
+                        pageName: 'Store Details',
+                    },
+                    name: 'storeDetails',
+                    props: true
+                }
+            ]
+        },
+        {
+            
+            path: '/404',
+            name: '404',
+            component: view('notfoundcomponent')
+        },
+        {
+            path: '*',
+            redirect: '/404'
+        }
+    ]
+
+    /*
+    * Asynchronously load view (lazy-loading)
+    * @param {string} name the filename (basename) of the view to load.
+    */
+    function view(name) {
+        return function(resolve) { require(['vue!' + name + '.vue'], resolve); }
+    };
+});
