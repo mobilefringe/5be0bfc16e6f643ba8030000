584869a6d1e4a6b0c90f1ac569af1c84d9c756b1
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-21 12:55:50 -0500

    Coral Ridge 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -46,7 +46,7 @@
             methods: {
                 updateCurrentPage(id) {
                     var _this = this;
-                    console.log("this.property.mm_host")
+                    console.log("this.property.mm_host",this.property.mm_host)
                     this.$store.dispatch('LOAD_PAGE_DATA', { url: this.property.mm_host + "/pages/" + this.id + ".json" }).then(function (response) {
                         _this.currentPage = response.data;
                         _this.dataLoaded = true;
