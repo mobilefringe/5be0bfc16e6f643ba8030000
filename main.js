c32759ab639fe1f5d74a7e451ea67fead5388401
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-21 12:54:58 -0500

    Coral Ridge 1.0  /main.js

--- main.js
+++ main.js
@@ -172,7 +172,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
                     await this.$store.dispatch("getData","property");
                     this.property.mm_host = this.property.mm_host.replace("http:", "");   
                     
-                    let results = await Promise.all([this.$store.dispatch("INITIALIZE_LOCALE"), this.$store.dispatch("getData", "property"), this.$store.dispatch("getData", "hours"), this.$store.dispatch("getData", "stores")]);
+                    let results = await Promise.all([this.$store.dispatch("INITIALIZE_LOCALE"), this.$store.dispatch("getData", "hours"), this.$store.dispatch("getData", "stores")]);
                     
                      await Promise.all([this.$store.dispatch("LOAD_META_DATA")]);
                      
