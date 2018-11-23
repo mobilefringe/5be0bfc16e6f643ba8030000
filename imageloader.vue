f8c816833ea6405ebb2fd7b4962c60e2f9825d75
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-23 12:14:05 -0500

    Coral Ridge 1.0  /imageloader.vue

--- imageloader.vue
+++ imageloader.vue
@@ -2,29 +2,7 @@
     <div> <!-- for some reason if you do not put an outer container div this component template will not render -->
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
-            <div v-if="dataLoaded" v-cloak>
-                <inside-header-component pageName="Location"></inside-header-component>
-                <div class="main_container margin_30">
-                    <div class="details_row">
-                        <div class="details_col_3">
-                            <h3 class="inside_page_title">Office Phone Number</h3>
-                            <a class="inside_page_link" :href="'tel:' + property.contact_phone">{{ property.contact_phone }}</a>
-                            <h3 class="inside_page_title">Address</h3>
-                            <p class="inside_page_link">
-                                {{ property.address1 }} <br>
-                                {{ property.city }}, {{ property.province_state }} <br>
-                                {{ property.postal_code }}
-                            </p>
-                            <a class="animated_btn" :href="siteInfo.googleMapsURL" target="_blank">Driving Direction</a>   
-                        </div>
-                        <div class="details_col_9">
-                            <iframe title="Map" width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  :src="'http://maps.google.nl/maps?q=' + getPropertyAddress + '&amp;hl=en&amp;ie=UTF8&amp;t=v&amp;hnear=' + getPropertyAddress + '&amp;z=16&amp;output=embed'"></iframe>
-                            <hr>
-                            <!--<div class="page_content" v-if="currentPage" v-html="currentPage.body"></div>-->
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
         </transition>
     </div>
 </template>
