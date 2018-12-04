3b4e7fbc4a82afd6e72bbc392ddd25d9399afe4c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-12-04 16:34:16 -0500

    Coral Ridge 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -139,9 +139,9 @@
                     this.updateCurrentStore(this.$route.params.id);
                 },
                 currentStore: function () {
-                    if (_.includes(this.currentStore.store_front_url_abs, 'missing')) {
-                        this.currentStore.store_front_url_abs = "http://placehold.it/600x600"
-                    }
+                    // if (_.includes(this.currentStore.store_front_url_abs, 'missing')) {
+                    //     this.currentStore.store_front_url_abs = "http://placehold.it/600x600"
+                    // }
                     this.currentStore.zoom = 2;
                     
                     if (_.includes(this.currentStore.store_front_url_abs, 'missing')) {
