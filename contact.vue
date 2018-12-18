5cc2d3d836a43eec785682784f65ed36f43c654e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-12-18 15:15:29 -0500

    Coral Ridge 1.0  /contact.vue

--- contact.vue
+++ contact.vue
@@ -17,7 +17,7 @@
                                 <form class="form-horizontal clearfix" action="form-submit" v-on:submit.prevent="validateBeforeSubmit">
                                     <div class="col-xs-12 col-md-6" :class="{'has-error': errors.has('name')}">
                                         <label for="name">Name</label>
-                                        <input id="name" v-model="form_data.name" v-validate="'required|true'" class="form-control" :class="{'input': true}" name="name" type="text" data-vv-delay="1000">
+                                        <input id="name" v-model="form_data.name" v-validate="'required:true'" class="form-control" :class="{'input': true}" name="name" type="text" data-vv-delay="1000">
                                         <span v-show="errors.has('name')" class="form-control-feedback">{{ errors.first('name') }}</span>
                                     </div>
                                     <div class="col-xs-12 col-md-6" :class="{'has-error': errors.has('email')}">
