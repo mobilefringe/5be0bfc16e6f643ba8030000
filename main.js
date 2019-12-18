243b6b02ea0b3bd41d6e96b43f2ecf1ce31d64f7
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-12-17 21:20:11 -0500

    Coral Ridge 1.0  /main.js

--- main.js
+++ main.js
@@ -53,10 +53,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
     Vue.use(VueRouter);
     Vue.use(Vue2Filters);
     Vue.use(VueI18n);
-    
-    // const observer = lozad(); // lazy loads elements with default selector as '.lozad'
-    // observer.observe();
-    
+
     /* initialize router */
     const router = new VueRouter({
         mode: 'history',
