d7652a95d162780265dc6d8edc387b9627189e5d
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-03-13 11:20:54 -0400

    Coral Ridge 1.0  /covid-19-update.vue

--- covid-19-update.vue
+++ covid-19-update.vue
@@ -63,16 +63,27 @@
                         console.error( "Could not retrieve data from server. Please check internet connection and try again.");
                         _this.$router.replace({ name: '404' });
                     });
+                    
                     this.loadData().then(response => {
-                        var temp_repo1 = this.findRepoByName('Sales & Events Side Banner');
-                        if(temp_repo1 && temp_repo1.images) {
-                            this.sideBanner = temp_repo1.images[0];
-                        } else {
-                            this.sideBanner = {
-                                "image_url": ""
-                            }
-                        } 
+                        var temp_repo = this.findRepoByName('COVID-19 Banner');
+                        if(temp_repo) {
+                            this.pageBanner = temp_repo.images[0];
+                        }
+                        else {
+                            this.pageBanner = {};
+                            this.pageBanner.image_url = "";
+                        }
                     });
+                    // this.loadData().then(response => {
+                    //     var temp_repo1 = this.findRepoByName('Sales & Events Side Banner');
+                    //     if(temp_repo1 && temp_repo1.images) {
+                    //         this.sideBanner = temp_repo1.images[0];
+                    //     } else {
+                    //         this.sideBanner = {
+                    //             "image_url": ""
+                    //         }
+                    //     } 
+                    // });
                     
                
                 },
