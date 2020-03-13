bc164b51376604851ca47763a7a785d1421c234a
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-03-13 12:51:41 -0400

    Coral Ridge 1.0  /covid-19.vue

--- covid-19.vue
+++ covid-19.vue
@@ -7,7 +7,7 @@
                     
                 <!--</div>-->
                 <div class="inside_header_background covid-background"  v-if="pageBanner" :style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
-                    <div class="main_container center_txt">
+                    <div class="main_container center_text">
                         <div class="page_container">
                             <h1>{{currentPage.title}} <span>Coronavirus information & Resources</span></h1>
                             
@@ -138,6 +138,10 @@
     }
     .covid-page {
         font-family: 'Maven Pro', sans-serif;
+    }
+    .center_text {
+        display: flex;
+        align-items: center
     }
 
     
