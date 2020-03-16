7386aa1d02dc72aa82324f8226bf38aa1bb069dc
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-03-16 17:26:53 -0400

    Coral Ridge 1.0  /covid-19.vue

--- covid-19.vue
+++ covid-19.vue
@@ -3,17 +3,16 @@
         <loading-spinner v-if="!dataLoaded"></loading-spinner>
         <transition name="fade">
             <div v-if="dataLoaded" v-cloak>
+                <div class="page_header" v-if="pageBanner" v-bind:style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
+        		</div>
                 <!--<div class="inside_header_background covid-background"  v-if="pageBanner" :style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">-->
-                    
-                <!--</div>-->
-                <div class="inside_header_background covid-background"  v-if="pageBanner" :style="{ backgroundImage: 'url(' + pageBanner.image_url + ')' }">
-                    <div class="main_container center_text">
-                        <div class="page_container">
-                            <h1><span>{{currentPage.title}} </span>Coronavirus information & Resources</h1>
+                <!--    <div class="main_container center_text">-->
+                <!--        <div class="page_container">-->
+                <!--            <h1><span>{{currentPage.title}} </span>Coronavirus information & Resources</h1>-->
                             
-                        </div>
-                    </div>
-                </div>
+                <!--        </div>-->
+                <!--    </div>-->
+                <!--</div>-->
                 
                 <div class="main_container mobile_padding margin_30">
                     <div class="">
