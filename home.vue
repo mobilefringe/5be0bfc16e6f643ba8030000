8ca0850dd2b452796726aa1f1be875b5d64ef514
mithun@mobilefringe.com @ CodeCloud <mithun@mobilefringe.com @ CodeCloud>
2020-05-13 17:14:08 -0400

    Coral Ridge 1.1  /home.vue

--- home.vue
+++ home.vue
@@ -32,6 +32,24 @@
     define(["Vue", "vuex", "vue-meta", "vue!vue-slick"], function (Vue, Vuex, Meta, slick) {
         return Vue.component("home-component", {
             template: template, // the variable template will be injected
+            head: {
+                // To use "this" in the component, it is necessary to return the object through a function
+                title: function () {
+                  return {
+                    inner: this.meta.meta_title,
+                    separator: ' ', // Leave empty separator
+                    complement: ' ' // Leave empty complement
+                  }
+                },
+                meta: function () {
+                  return [
+                     { name: 'description', id: 'description', content: this.meta.meta_description },
+                     { name: 'keywords',  id: 'keywords', content: this.meta.meta_keywords },
+                     { property: 'og:title', id: 'og:title', content: this.meta.meta_title },
+                     { property: 'og:description', id: 'og:description', content: this.meta.meta_description }
+                  ]
+                }
+            },
             data: function() {
                 return {
                     dataLoaded: false,
